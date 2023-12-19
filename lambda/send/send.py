7ebdba7bcb4c890037077913a253bdef3a4cc6cb
Darlei Randel <darley_rom@hotmail.com>
2023-12-18 21:37:04 -0300

    add s3 get

--- lambda/send/send.py
+++ lambda/send/send.py
@@ -1,18 +1,32 @@
 import boto3 
 import os
 import json
-from datetime import datetime
+import urllib.parse
+from pprint import pprint
 
 def lambda_handler(event, context):
+
+    s3 = boto3.client('s3')
+
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    filename = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+
+    file = s3.get_object(Bucket='python-sync', Key='Área de Trabalho/test.json')
+
+    text = file['Body'].read().decode()
+    data = json.loads(text)
+
+    print(data)
+
+def send(data):
+    
     sqs = boto3.client('sqs')
-    now = datetime.now() 
-    current = now.strftime("%H:%M:%S") 
      
     sqs.send_message(
         QueueUrl = os.environ.get("QUEUEURL"),
         MessageGroupId = '1',
-        MessageBody = current)
+        MessageBody = data)
     return {
         'statusCode': '200',
-        'body': json.dumps(current)
+        'body': json.dumps(data)
     }
